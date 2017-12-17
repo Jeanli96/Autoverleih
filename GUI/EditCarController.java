5edf9ee8dc240e8bc281b76d267e517194cceda3
Meowar1 <34512182+Meowar1@users.noreply.github.com>
2017-12-17 21:51:51 +0100

    Update EditCarController.java

--- GUI/EditCarController.java
+++ GUI/EditCarController.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -32,15 +33,15 @@ public class EditCarController implements Initializable {
     private Button abbruch;
 
     @FXML
-    private TextField kennzeichen;
+    private Label kennzeichen;
     @FXML
-    private TextField marke;
+    private Label marke;
     @FXML
-    private TextField modell;
+    private Label modell;
     @FXML
-    private ChoiceBox fahrzeugtyp;
+    private Label fahrzeugtyp;
     @FXML
-    private TextField sitzplaetze;
+    private Label sitzplaetze;
     @FXML
     private TextField farbe;
     @FXML
@@ -48,8 +49,6 @@ public class EditCarController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<String> daten = FXCollections.observableArrayList("Kleinwagen", "Van", "Kombi", "Transporter", "Limousine");
-        fahrzeugtyp.setItems(daten);
 
         EditCarStage stage = EditCarStage.stage;
         sAuto = stage.getAuto();
@@ -57,53 +56,16 @@ public class EditCarController implements Initializable {
         kennzeichen.setText(sAuto.getKennzeichen());
         marke.setText(sAuto.getMarke());
         modell.setText(sAuto.getModell());
+        fahrzeugtyp.setText(sAuto.getTyp());
         sitzplaetze.setText("" + sAuto.getSitzplaetze());
         farbe.setText(sAuto.getFarbe());
-        tagessatz.setText("" + sAuto.getTagessatz());
-
-        switch (sAuto.getModell()) {
-            case "Kleinwagen":
-                fahrzeugtyp.getSelectionModel().select(0);
-                break;
-            case "Van":
-                fahrzeugtyp.getSelectionModel().select(1);
-                break;
-            case "Kombi":
-                fahrzeugtyp.getSelectionModel().select(2);
-                break;
-            case "Transporter":
-                fahrzeugtyp.getSelectionModel().select(3);
-                break;
-            case "Limousine":
-                fahrzeugtyp.getSelectionModel().select(4);
-                break;
-        }
+        tagessatz.setText("" + sAuto.getTagessatz());        
     }
     
     @FXML
     private void buttonBearbeiten() {
 
-        String holder = "";
-
-        switch (fahrzeugtyp.getSelectionModel().getSelectedIndex()) {
-            case 0:
-                holder = "Kleinwagen";
-                break;
-            case 1:
-                holder = "Van";
-                break;
-            case 2:
-                holder = "Kombi";
-                break;
-            case 3:
-                holder = "Transporter";
-                break;
-            case 4:
-                holder = "Limousine";
-                break;
-        }
-
-        Auto newAuto = new Auto(kennzeichen.getText(), marke.getText(), Integer.parseInt(sitzplaetze.getText()), Float.parseFloat(tagessatz.getText()), modell.getText(), holder, farbe.getText(), "keine");
+        Auto newAuto = new Auto(kennzeichen.getText(), marke.getText(), Integer.parseInt(sitzplaetze.getText()), Float.parseFloat(tagessatz.getText()), modell.getText(), fahrzeugtyp.getText(), farbe.getText(), "keine");
         new AutoQuery().edit(newAuto);
 
         FXMLMain.data.fullUpdate();
